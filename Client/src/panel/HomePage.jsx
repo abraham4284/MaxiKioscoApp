d904828b9747016502679dacc98c5097869d7060
Abraham <abraham4284@hotmail.com>
2024-08-22 00:21:21 -0300

    Elimine rect-player

--- Client/src/panel/HomePage.jsx
+++ Client/src/panel/HomePage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactPlayer from "react-player";
 import { DescripcionSistema } from "./HomeDescription/DescripcionSistema";
 
 export const HomePage = () => {
@@ -9,14 +8,6 @@ export const HomePage = () => {
       <div className="row ">
         <div className="col-12 text-center mt-5">
            <h3>Video explicativo</h3>
-        </div>
-        <div className="col-12 d-flex justify-content-center mt-5 ">
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
-            controls
-            width={854}
-            height={480}
-          />
         </div>
       </div>
     </div>
