714656a825fe76b832acd4e24a9a20c7c78ad832
Abraham <abraham4284@hotmail.com>
2024-08-22 01:39:12 -0300

    Probando

--- src/routes/proveedores.routes.js
+++ src/routes/proveedores.routes.js
@@ -11,12 +11,19 @@ import { authRequired } from "../middlewares/ValidarToken.js";
 
 const router = Router();
 
-router.get('/proveedores',authRequired, getProvedores);
-router.get('/proveedores/:id',authRequired, getProvedorestId);
+router.get('/proveedores', getProvedores);
+router.get('/proveedores/:id', getProvedorestId);
 
-router.post('/proveedores', authRequired, createProvedores);
-router.put('/proveedores/:id', authRequired, updateProvedores);
-router.delete('/proveedores/:id', authRequired, deleteProvedores)
+router.post('/proveedores',  createProvedores);
+router.put('/proveedores/:id',  updateProvedores);
+router.delete('/proveedores/:id',  deleteProvedores)
+
+// router.get('/proveedores',authRequired, getProvedores);
+// router.get('/proveedores/:id',authRequired, getProvedorestId);
+
+// router.post('/proveedores', authRequired, createProvedores);
+// router.put('/proveedores/:id', authRequired, updateProvedores);
+// router.delete('/proveedores/:id', authRequired, deleteProvedores)
 
 
 export default router;
