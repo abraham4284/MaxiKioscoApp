59ed0eb9ee858305db8f7881ac487a38819dd9bd
Abraham <abraham4284@hotmail.com>
2024-08-23 02:09:54 -0300

    Probando

--- Client/src/components/NavbarSocial.jsx
+++ Client/src/components/NavbarSocial.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./styles.css";
-import { instagram, email, github, linkedin } from "../helpers/social";
-import CV from '../../public/AbrahamApasFullStackDeveloper.pdf'
+// import "./styles.css";
+import { instagram, github, linkedin, portafolio } from "../helpers/social";
+import CV from '../../public/AbrahamApasFullStackDeveloper.pdf';
+
 
 export const NavbarSocial = () => {
-
-  
-
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      {/* Container wrapper */}
       <div className="container">
         <Link className="navbar-brand text mb-1" to="/">
           <i className="fa-solid fa-code" style={{ color: "#00fca8" }}></i>{" "}
@@ -19,12 +16,11 @@ export const NavbarSocial = () => {
           </b>
         </Link>
 
-        {/* Toggle button */}
         <button
-          className="navbar-toggler text-white"
+          className="navbar-toggler"
           type="button"
-          data-mdb-collapse-init
-          data-mdb-target="#navbarButtonsExample"
+          data-bs-toggle="collapse" // Cambiado de "data-mdb-toggle"
+          data-bs-target="#navbarButtonsExample"
           aria-controls="navbarButtonsExample"
           aria-expanded="false"
           aria-label="Toggle navigation"
@@ -32,9 +28,7 @@ export const NavbarSocial = () => {
           <i className="fas fa-bars"></i>
         </button>
 
-        {/* Collapsible wrapper */}
         <div className="collapse navbar-collapse" id="navbarButtonsExample">
-          {/* Left links */}
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link className="nav-link" to="/login">
@@ -47,11 +41,16 @@ export const NavbarSocial = () => {
               </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link btn btn-cv" href={CV} download="AbrahamApasFullStackDeveloper">Descargar CV</a>
+              <a
+                className="nav-link btn btn-cv"
+                href={CV}
+                download="AbrahamApasFullStackDeveloper"
+              >
+                Descargar CV
+              </a>
             </li>
           </ul>
 
-          {/* Icons */}
           <ul className="navbar-nav d-flex flex-row nav-social">
             <li className="nav-item me-3 me-lg-0">
               <a className="nav-link" href={linkedin} target="_blank">
@@ -68,17 +67,14 @@ export const NavbarSocial = () => {
                 <i className="fab fa-instagram text-white"></i>
               </a>
             </li>
-            
             <li className="nav-item me-3 me-lg-0">
-              <a className="nav-link" href="#">
-              <i className="fa-solid fa-briefcase text-white"></i>
+              <a className="nav-link" href={portafolio} target="_blank">
+                <i className="fa-solid fa-briefcase text-white"></i>
               </a>
             </li>
           </ul>
         </div>
-        {/* Collapsible wrapper */}
       </div>
-      {/* Container wrapper */}
     </nav>
   );
 };
--- Client/src/helpers/social.js
+++ Client/src/helpers/social.js
@@ -4,4 +4,4 @@ export let linkedin =
   "https://www.linkedin.com/in/abraham-adrian-apas-8294a1209?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
 export let github = "https://github.com/abraham4284";
 export let email = "abraham4284@hotmail.com";
-export let portafolio = "";
+export let portafolio = "https://abraham-apas.onrender.com";
