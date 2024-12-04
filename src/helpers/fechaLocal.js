39a44e9723c55ddfcbde4c5d3a36f1c4d8fe9388
Abraham <abraham4284@hotmail.com>
2024-12-03 23:43:28 -0300

    Fecha Local

--- src/helpers/fechaLocal.js
+++ src/helpers/fechaLocal.js
@@ -1,20 +1,15 @@
 export const fechaLocal = () => {
-    try {
-      let hoy = new Date();
-      hoy.setMinutes(hoy.getMinutes() - hoy.getTimezoneOffset());
-      let HoyfechaLocal = hoy.toISOString().slice(0, 10);
-  
-      return {
-        HoyfechaLocal,
-      };
-    } catch (error) {
-      console.log({
-        error: error.message,
-        message: "Error en fechaLocal /helpers",
-      });
-    }
-  };
+  try {
+    const ahoraUTC = new Date();
+    const zonaHorariaOffset = -3;
+    ahoraUTC.setHours(ahoraUTC.getHours() + zonaHorariaOffset);
 
-const fecha = fechaLocal();
-
-console.log(fecha)
+    const HoyfechaLocal = ahoraUTC.toISOString().slice(0, 10);
+    return { HoyfechaLocal };
+  } catch (error) {
+    console.log({
+      error: error.message,
+      message: "Error en fechaLocal /helpers",
+    });
+  }
+};
